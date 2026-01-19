593f84fef02e521958cb00eaf35958cd39bb68ce
ilyx-me <ljenlzqvlbxh@ilyx.me>
2026-01-19 03:22:38 +0000

    fix: tpm early unlock broken message

--- script/systemd/cryptsetupEarly.sh
+++ script/systemd/cryptsetupEarly.sh
@@ -5,11 +5,9 @@ device_mapper=$2
 device_disk=$3
 flags=$4
 
-succeed="true"
+"$systemd_path"/bin/systemd-cryptsetup attach "$device_mapper" /dev/disk/by-partlabel/"$device_disk" - "$flags"
 
-("$systemd_path"/bin/systemd-cryptsetup attach "$device_mapper" /dev/disk/by-partlabel/"$device_disk" - "$flags") || (succeed="false")
-
-if [ "$succeed" = "true" ]; then
+if [ $? -ne 0 ]; then
     if plymouth --ping || false; then
         plymouth display-message --text="Unlocking drive automatically failed. Proceeding to manual decryption."
         sleep 1
